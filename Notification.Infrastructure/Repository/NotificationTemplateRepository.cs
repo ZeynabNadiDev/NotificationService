c6c90b1dbc438435f013ec5dd748f163275897a3
ZeynabNadiDev <znadi05@gmail.com>
2026-06-06 19:50:09 +0330

    feat(infrastructure): implement NotificationTemplateRepository and NotificationRepository
    - Implemented EF Core repositories for Notification and NotificationTemplate
    - Added pagination, unread count and mark-as-read logic
    - Added secure MarkUserNotificationAsReadAsync method
    - Integrated with UnitOfWork pattern

---
+++ Notification.Infrastructure/Repository/NotificationTemplateRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notification.Domain.Entities;
+using Notification.Domain.Enums;
+using Notification.Domain.Interfaces.Repository;
+using Notification.Infrastructure.NotificationContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notification.Infrastructure.Repository
+{
+    public class NotificationTemplateRepository : INotificationTemplateRepository
+    {
+        private readonly NotificationDbContext _context;
+        public NotificationTemplateRepository(NotificationDbContext context)
+        {
+            _context = context;
+        }
+        async Task<NotificationTemplate?> INotificationTemplateRepository.GetByIdAsync(Guid id)
+        {
+            return await _context.NotificationTemplates.FindAsync(id);
+        }
+
+        async Task<NotificationTemplate?> INotificationTemplateRepository.GetByTypeAndChannelAsync(
+          NotificationType type, NotificationChannel channel)
+        {
+            return await _context.NotificationTemplates
+                       .FirstOrDefaultAsync(x => x.TemplateType == type && x.Channel == channel);
+        }
+
+        async Task<List<NotificationTemplate>> INotificationTemplateRepository.GetAllAsync()
+        {
+            return await _context.NotificationTemplates
+               .OrderBy(x => x.TemplateType)
+               .ThenBy(x => x.Channel)
+               .ToListAsync();
+        }
+        async Task INotificationTemplateRepository.AddAsync(NotificationTemplate template)
+        {
+            await _context.NotificationTemplates.AddAsync(template);
+        }
+
+        Task INotificationTemplateRepository.UpdateAsync(NotificationTemplate template)
+        {
+            _context.NotificationTemplates.Update(template);
+            return Task.CompletedTask;
+        }
+    }
+}
